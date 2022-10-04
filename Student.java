87c0b9d4b6842425f1e710ad1d1dfac68dc16537
Dunlayawit Panklang <dunlayawit.dev@gmail.com>
2022-10-04 15:03:08 +0700

    commit

---
+++ Student.java
@@ -0,0 +1,31 @@
+public class Student{
+    private String id, name;
+    private double gpa;
+
+    //Method for set Details 
+    public void setDetails(String ID, String n, double GPA){
+        id = ID;
+        name = n;
+        gpa = GPA;
+    }
+
+    //Method for show Details 
+    public void showDetails(){
+        System.out.println("ID : "+id);
+        System.out.println("Name : "+name);
+        System.out.println("GPA : "+gpa);
+    }
+
+    //Main Method
+    public static void main(String args[]){
+        Student sc1 = new Student();
+        Student sc2 = new Student();
+
+        sc1.setDetails("615-2500-001", "DUN", 3.45);
+        sc2.setDetails("615-2500-002", "AYA", 3.86);
+
+        sc1.showDetails();
+        System.out.println();
+        sc2.showDetails();
+    }
+}
